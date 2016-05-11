d4e45fa6b5c63c9bc5894356c923f57ebfb7ad1c
zgibanez <zoiletum@gmail.com>
2016-05-11 12:19:56 +0200

    Update cross_detection.cpp

--- cross_detection.cpp
+++ cross_detection.cpp
@@ -19,10 +19,11 @@ int main(int argc, char** argv)
 
 	///-- STEP 1 : Extract features from template
 
-	//load template
+	//load template and blur it
 	Mat img = imread(argv[1], 0);
+	GaussianBlur(img, img, Size(3, 3), 4);
 
-	if (img.empty()){
+	if (img.empty()) {
 		cout << "can't read image" << endl;
 		return -1;
 	}
@@ -32,7 +33,7 @@ int main(int argc, char** argv)
 	Mat descriptors_1, descriptors_2;
 
 	//set feature detector
-	Ptr<SiftFeatureDetector> detector = SiftFeatureDetector::create(0,3,0.04,10,1.6);
+	Ptr<SiftFeatureDetector> detector = SiftFeatureDetector::create(0, 3, 0.04, 10, 0.8);
 	detector->detect(img, keypoints_1);
 
 	//set descriptor extractor
@@ -48,21 +49,22 @@ int main(int argc, char** argv)
 	/// -- STEP 2 : Load video and binarize frame
 
 	//load video
-	VideoCapture capture("C:/images/video3.mp4");
+	VideoCapture capture("C:/images/video4.mp4");
 	if (!capture.isOpened())
 		throw "Error: cannot read video";
 
 	for (; ; )
 	{
 		Mat frame, frame_gray, frame_thresh;
-		
+
 		//load frame, binarize it
 		capture >> frame;
 		if (frame.empty()) break;
 
 		GaussianBlur(frame, frame, Size(3, 3), 4);
+		addWeighted(frame, 1.5, frame, -0.5, 0, frame);
 		cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
-		adaptiveThreshold(frame_gray, frame_thresh, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 11, 2);
+		adaptiveThreshold(frame_gray, frame_thresh, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 15, 2);
 		imshow("Adaptative threshold", frame_thresh);
 
 		///-- STEP 3: Find in the thresholded frame the mark's outline with shape detection
@@ -73,50 +75,65 @@ int main(int argc, char** argv)
 		/// -- STEP 4 : Extract features of the ROI
 		detector->detect(ROI, keypoints_2);
 		extractor->compute(ROI, keypoints_2, descriptors_2);
+		cout << "Keypoints de frame computados" << endl;
 
 		/// -- STEP 5 : Find and filter matches
 		BFMatcher matcher;
 
-		//Filter 1: Lowe's ratio
-		vector<vector<DMatch>> matches;
-		matcher.knnMatch(descriptors_1, descriptors_2, matches, 5);
-		vector<DMatch> good_matches;
-		const float ratio = 0.8;
-
-		for (int i = 0; i < matches.size(); i++)
+		//if the frame descriptor is empty
+		//skip matching
+		if (!descriptors_2.empty())
 		{
-			if (matches[i][0].distance < ratio * matches[i][1].distance)
+			//Filter 1: Lowe's ratio
+			std::vector<vector<cv::DMatch>> matches;
+			matcher.knnMatch(descriptors_1, descriptors_2, matches, 5);
+			vector<DMatch> good_matches;
+			const float ratio = 0.8;
+			for (int i = 0; i < matches.size(); i++)
 			{
-				good_matches.push_back(matches[i][0]);
+				if (matches[i][0].distance < ratio * matches[i][1].distance)
+				{
+					good_matches.push_back(matches[i][0]);
+				}
+
+			}
+			cout << good_matches.size() << " puntos filtrados con Lowe's Ratio " << endl;
+
+
+			//Filter 2: Cross checking
+			vector<DMatch> matches12, matches21, good_matches2;
+			matcher.match(descriptors_1, descriptors_2, matches12);
+			matcher.match(descriptors_2, descriptors_1, matches21);
+
+			for (size_t i = 0; i < matches12.size(); i++)
+			{
+				DMatch forward = matches12[i];
+				DMatch backward = matches21[forward.trainIdx];
+				if (backward.trainIdx == forward.queryIdx)
+					good_matches2.push_back(forward);
 			}
 
+			cout << good_matches2.size() << " puntos filtrados con Cross-checking " << endl;
+
+			//Filter 3: Symmetry Test
+
+			///--Step 6: Present data
+			Mat img_matches;
+			drawMatches(img, keypoints_1, ROI, keypoints_2, good_matches2, img_matches, Scalar(0, 255, 0), Scalar(0, 255, 0),
+				vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+			imshow("matches", img_matches);
+			imshow("frame", frame);
+
+			//empty vectors
+			good_matches.clear();
+			good_matches2.clear();
+			matches.clear();
+			keypoints_2.clear();
 		}
-
-		cout << good_matches.size() << " puntos filtrados con Lowe's Ratio " << endl;
-
-
-		//Filter 2: Cross checking
-		vector<DMatch> matches12, matches21, good_matches2;
-		matcher.match(descriptors_1, descriptors_2, matches12);
-		matcher.match(descriptors_2, descriptors_1, matches21);
-
-		for (size_t i = 0; i < matches12.size(); i++)
-		{
-			DMatch forward = matches12[i];
-			DMatch backward = matches21[forward.trainIdx];
-			if (backward.trainIdx == forward.queryIdx)
-				good_matches2.push_back(forward);
-		}		
-
-		cout << good_matches2.size() << " puntos filtrados con Cross-checking " << endl;
-		
-		//Filter 3: Symmetry Test
-
-		///--Step 6: Present data
-		Mat img_matches;
-		drawMatches(img, keypoints_1, ROI, keypoints_2, good_matches, img_matches, Scalar(0,255,0), Scalar(0, 255, 0),
-			vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-		imshow("matches", img_matches);
+		else {
+			cout << "El descriptor del frame estaba vacio" << endl;
+			imshow("ROI", ROI);
+		}
 
 		//exit with ESC key
 		if (waitKey(-1) == 27)
@@ -138,14 +155,22 @@ Mat FindMark(Mat * thresh, Mat * frame)
 	Rect rectBoundedToMark;
 	Mat frame_thresh_copy = thresh->clone();
 
+	//preprocess image
+	dilate(frame_thresh_copy, frame_thresh_copy, getStructuringElement(MORPH_ELLIPSE, Size(3, 3)));
+	erode(frame_thresh_copy, frame_thresh_copy, getStructuringElement(MORPH_ELLIPSE, Size(3, 3)));
+	GaussianBlur(frame_thresh_copy, frame_thresh_copy, Size(3, 3), 4);
+
 	//find contours on the frame
-	findContours(frame_thresh_copy.clone(), cnts, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
-	cout << cnts.size() << " contornos detectados" << endl;
+	Mat canny;
+	cout << " Buscando contornos... ";
+	Canny(frame_thresh_copy.clone(), canny, 250, 250 * 3);
+	findContours(canny, cnts, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
+	cout << cnts.size() << "  contornos detectados" << endl;
 
 	//approximate contours to poligons
 	int i;
 	double peri;
-	float MinArea = 100;
+	float MinArea = 300;
 	bool ROI_found = false;
 
 	for (i = 0; i < cnts.size(); i++) {
@@ -155,7 +180,7 @@ Mat FindMark(Mat * thresh, Mat * frame)
 		//find the contour with 4 sides and the biggest area
 		Moments M;
 		M = moments(cnts[i], true);
-		if (poligon.size() == 4 || poligon.size() == 5)
+		if (poligon.size() == 4)
 		{
 			if (M.m00 > MinArea) {
 				best_candidate = poligon;
@@ -165,13 +190,29 @@ Mat FindMark(Mat * thresh, Mat * frame)
 		}
 	}
 
+	//draw points
+	if (!best_candidate.empty()) {
+		for (i = 0; i < best_candidate.size(); i++) {
+			putText(*frame, "P", Point(best_candidate[i].x, best_candidate[i].y), FONT_HERSHEY_SIMPLEX, 0.3, Scalar(0, 255, 0));
+		}
+		line(*frame, best_candidate[0], best_candidate[1], Scalar(255, 0, 0));
+		line(*frame, best_candidate[1], best_candidate[2], Scalar(255, 0, 0));
+		line(*frame, best_candidate[2], best_candidate[3], Scalar(255, 0, 0));
+		line(*frame, best_candidate[3], best_candidate[0], Scalar(255, 0, 0));
+	}
+
 	//Return a Rect enclosing the biggest rectangle
+	//and prevent assertion errors
 	//if there is no candidate
 	//search on the whole image
 	Mat frame_copy = frame->clone();
 	rectBoundedToMark = boundingRect(best_candidate);
-	if (ROI_found)
+	if (ROI_found) {
+		cout << "Marca encontrada" << endl;
 		return frame_copy(rectBoundedToMark);
-	else
+	}
+	else {
+		cout << "Marca no encontrada" << endl;
 		return frame_copy;
+	}
 }
